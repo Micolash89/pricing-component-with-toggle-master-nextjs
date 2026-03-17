0044655be9224f03370fbc3d9764aa60c63f4b60
Micolash89 <espindolajavier2013@gmail.com>
2026-03-17 20:01:29 -0300

    `Refactored layout and pricing component`

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,44 @@
-
 export default function Home() {
-  return <>
+  return (
+    <>
+      <header>
+        <h1>Our Pricing</h1>
 
-<header>
-    Our Pricing
-  Annually
-  Monthly
-  </header>  
+        <section>Annually Monthly</section>
+      </header>
 
-  <main>
+      <main>
+        <section>
+          <h3>Basic</h3>
 
-<section>
+          <h4>&dollar;19.99</h4>
+          <h4>&dollar;199.99</h4>
 
-  Basic
-  &dollar;19.99
-  &dollar;199.99
-  500 GB Storage
-  2 Users Allowed
-  Send up to 3 GB
-  Learn More
-</section>
+          <span>500 GB Storage</span>
+          <span>2 Users Allowed</span>
+          <span>Send up to 3 GB</span>
 
-<section>
+          <button>Learn More</button>
+        </section>
 
-  Professional
-  &dollar;24.99
-  &dollar;249.99
-  1 TB Storage
-  5 Users Allowed
-  Send up to 10 GB
-  Learn More
-</section>
+        <section>
+          Professional &dollar;24.99 &dollar;249.99 1 TB Storage 5 Users Allowed
+          Send up to 10 GB Learn More
+        </section>
 
-<section>
+        <section>
+          Master &dollar;39.99 &dollar;399.99 2 TB Storage 10 Users Allowed Send
+          up to 20 GB Learn More
+        </section>
+      </main>
 
-  Master
-  &dollar;39.99
-  &dollar;399.99
-  2 TB Storage
-  10 Users Allowed
-  Send up to 20 GB
-  Learn More
-</section>
-  </main>
-
-  <footer className="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </footer>
-  
-  </>;
+      <footer className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </>
+  );
 }
